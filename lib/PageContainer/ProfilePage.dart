f30538831e9e085bbef806e1af94ff8796268a5d
Gustavo Romanha <gustavoromanha@live.com>
2019-11-06 07:08:39 -0300

    Profile page main structure changed. Rendering overflow solved.

--- lib/PageContainer/ProfilePage.dart
+++ lib/PageContainer/ProfilePage.dart
@@ -7,74 +7,80 @@ final Color green = Color(0xFF1E8161);
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.only(top: 16),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 2,
-          decoration: BoxDecoration(
-            color: Colors.transparent,
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(32),
-                topLeft: Radius.circular(32),
-                bottomRight: Radius.circular(32),
-                bottomLeft: Radius.circular(32)),
-          ),
-          child: Column(
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 120,
-                    height: 120,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            fit: BoxFit.fill, image: NetworkImage(url))),
-                  ),
-                ],
+              Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        fit: BoxFit.fill, image: NetworkImage(url))),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 32),
-                child: Text(
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: <Widget>[
+                Text(
                   'Elon Musk',
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 24,
                       fontWeight: FontWeight.bold),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                      padding:
-                          const EdgeInsets.only(top: 10, bottom: 10, left: 10),
-                      child: Text(
-                        "I'm just an Alien.",
-                        style: TextStyle(color: Colors.white70),
-                      )),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                      padding:
-                          const EdgeInsets.only(top: 10, bottom: 10, left: 10),
-                      child: Text(
-                        "Meme necromancer.",
-                        style: TextStyle(color: Colors.white70),
-                      )),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
-          //YOU WILL NEVER FIND THIS COMMENT!!!! #freeBug
-        ),
-      ],
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 1, bottom: 2, right: 13),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Idade: 48 anos',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.normal),
+                ),
+                Text(
+                  'Mora em: Algum lugar',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.normal),
+                ),
+              ],
+            ),
+          ),
+          Card(
+            child: Text("Card"),
+          ),
+          Card(
+            child: Text("Card"),
+          ),
+          Card(
+            child: Text("Card"),
+          ),
+          Card(
+            child: Text("Card"),
+          ),
+          Card(
+            child: Text("Card"),
+          ),
+          SizedBox(
+            child: Container(),
+          ),
+        ],
+      ),
     );
   }
 }
